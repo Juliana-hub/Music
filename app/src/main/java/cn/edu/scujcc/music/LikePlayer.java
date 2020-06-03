e8259a9c59462382fe5e70650e04b263d1c254d7
yufangling <2601526201@qq.com>
2020-06-03 16:25:11 +0800

    播放列表和我喜欢界面

---
+++ app/src/main/java/cn/edu/scujcc/music/LikePlayer.java
@@ -0,0 +1,14 @@
+package cn.edu.scujcc.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LikePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like_player);
+    }
+}
